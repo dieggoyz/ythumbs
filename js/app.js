d34d157f2a05922e77ad74cf4161ce7295eabaf7
dieggoyz <diegoeffar@gmail.com>
2024-11-05 01:06:47 -0300

    📦 chore: minor tweak

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-const input = document.getElementById('url');
-const thumbnail = document.getElementById('thumbnail');
-const download = document.getElementById('download-button');
-const error = document.getElementById('error');
+const input = document.getElementById('url')
+const thumbnail = document.getElementById('thumbnail')
+const download = document.getElementById('download-button')
+const error = document.getElementById('error')
 
 function getVideoId(url) {
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-  const match = url.match(regExp);
-  return match && match[2].length === 11 ? match[2] : null;
+	const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
+	const match = url.match(regExp)
+	return match && match[2].length === 11 ? match[2] : null
 }
 
-input.addEventListener('keyup', (e) => {
-  const url = input.value;
-  const videoId = getVideoId(url);
-  thumbnail.src = videoId ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg` : '';
-  download.href = videoId ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg` : '';
-  download.download = videoId ? `${videoId}-thumbnail.jpg` : '';
-  download.style.display = videoId ? 'inline-block' : 'none';
-  error.style.display = url && !videoId ? 'block' : 'none';
-});
+input.addEventListener('input', (e) => {
+	const url = input.value
+	const videoId = getVideoId(url)
+	thumbnail.src = videoId ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg` : ''
+	download.href = videoId ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg` : ''
+	download.download = videoId ? `${videoId}-thumbnail.jpg` : ''
+	download.style.display = videoId ? 'inline-block' : 'none'
+	error.style.display = url && !videoId ? 'block' : 'none'
+})
